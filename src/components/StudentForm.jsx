46ec8e43e6d19ff84eef41b8fd25943b1e13ddaf
Devindi-u <panchalidevindi@gmail.com>
2025-11-05 12:17:53 +0530

    create student, load to backend

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,8 +1,42 @@
+import axios from "axios";
+import { useState } from "react"
 import { FcReadingEbook } from "react-icons/fc"
 
 export const  StudentForm = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [dob, setDob] = useState('');
+
+    const handleSaveStudent = async(e) => {
+        e.preventDefault();
+        alert("1");
+        try {
+            const student = {//BE       //FE
+                        first_name: firstName, 
+                        last_name: lastName, 
+                        email: email, 
+                        dob: dob
+            };
+
+            //create API
+            alert("2");
+            await axios.create(
+                {
+                    baseURL:'http://localhost:3000/api/v1/students',
+                    headers:{'Content-Type':'application/json'} 
+                }
+            ).post('/create',student).then(
+                (msg) => {alert(msg.statusText)}
+            );
+
+        } catch (error) {
+            console.error("Failed to save student", error);           
+        }
+    }
+
     return (
-        <form action="" className="bg-white rounded-lg shadow-md p-6 mb-6">
+        <form onSubmit={handleSaveStudent} className="bg-white rounded-lg shadow-md p-6 mb-6">
             <div className="flex">
                 <h1 className="text-2xl flex items-center gap-2 mb-4 font-semibold">
                     <FcReadingEbook/><span>Add New Student</span>
@@ -11,15 +45,19 @@ export const  StudentForm = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <input type="text" placeholder="First Name"
                     className="p-2 border rounded" required
+                    onChange={(e) => setFirstName(e.target.value)}
                 />
                 <input type="text" placeholder="Last Name"
                     className="p-2 border rounded" required
+                    onChange={(e) => setLastName(e.target.value)}
                 />
                 <input type="email" placeholder="Email"
                     className="p-2 border rounded" required
+                    onChange={(e) => setEmail(e.target.value)}
                 />
                 <input type="date" placeholder="DOB"
                     className="p-2 border rounded" required
+                    onChange={(e) => setDob(e.target.value)}
                 />
             </div>
             <button type="submit" className="bg-sky-500 px-4 py-2 rounded 
